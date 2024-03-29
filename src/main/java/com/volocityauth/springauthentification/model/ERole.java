4ccafeede6451b2f2980a120430796f3bcbfb1ac
Fulldia <camille.lafourcade64@orange.fr>
2024-03-29 14:42:28 +0100

    token jwt ok

---
+++ src/main/java/com/volocityauth/springauthentification/model/ERole.java
@@ -0,0 +1,7 @@
+package com.volocityauth.springauthentification.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/volocityauth/springauthentification/SpringAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volocityauth.springauthentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
